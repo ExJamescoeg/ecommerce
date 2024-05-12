cbf212871d6c2021a10447f13178fadf25094399
GenthaAliansi <genthaaliansi811@gmail.com>
2024-05-12 09:46:40 +0700

    membuat halaman sesuai desain figma

---
+++ src/views/ProductView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Products />
+  </div>
+</template>
+
+<script>
+import Products from "@/components/ProductCard";
+
+export default {
+  components: {
+    Products,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
